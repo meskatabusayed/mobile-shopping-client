1d79dea466a7305e34e5d6489c69b081fd8e8c5e
meskatabusayed <meskatabusayed@gmail.com>
2023-10-20 10:34:50 +0600

    Fundamentals section wrap up nicely

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,26 +16,22 @@ const Home = () => {
             <Banner></Banner>
             <MyBrand loadedBrands={loadedBrands}></MyBrand>
             <div>
-                <h2 className="text-center text-3xl font-bold mt-10 mb-10">Trusted By</h2>
-                <div  className="grid grid-cols-1 lg:grid-cols-2 gap-10 ml-5">
-                    <div>
-                        <img src="https://i.ibb.co/YkN5Qn4/trust-1.png" alt="" />
+                <h2 className="text-center text-3xl font-bold mt-10 mb-10">Best Seller Product Image</h2>
+                <div  className="grid grid-cols-1 lg:grid-cols-2 gap-4 ml-5">
+                    <div className="w-40">
+                        <img src="https://i.ibb.co/1KXwKLK/AppleP.webp" alt="" />
                     </div>
-                    <div>
-                        <img src="https://i.ibb.co/Qb8fjPh/trust-2.png" alt="" />
+                    <div className="w-40">
+                        <img src="https://i.ibb.co/XJvXT68/samP.webp" alt="" />
                     </div>
-                    <div>
-                        <img src="https://i.ibb.co/nQQp1WF/trust-3.png" alt="" />
+                    <div className="w-40">
+                        <img src="https://i.ibb.co/PwfjhcM/LenLap.webp" alt="" />
                     </div>
-                    <div>
-                        <img src="https://i.ibb.co/V2hpmM2/trust-4.png" alt="" />
+                    <div className="w-40">
+                        <img src="https://i.ibb.co/3ymp4gG/samT.webp" alt="" />
                     </div>
-                    <div>
-                        <img src="https://i.ibb.co/MZ4ksWS/trust-5.png" alt="" />
-                    </div>
-                    <div>
-                        <img src="https://i.ibb.co/kQhQ6z8/trust-6.png" alt="" />
-                    </div>
+
+                    
                 </div>
             </div>
 
